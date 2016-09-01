ce2dff493a77293017b66a07f5d0246b6d7fb78d
李林 <ww1095@163.com>
2016-09-01 17:37:28 +0800

    1

---
+++ RichTextDemo/src/com/andli/richtextdemo/RichTextDemoActivity.java
@@ -0,0 +1,15 @@
+package com.andli.richtextdemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.commonsware.cwac.richedit.demo.R;
+
+public class RichTextDemoActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
